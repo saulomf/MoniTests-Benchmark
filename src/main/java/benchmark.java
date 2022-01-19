29e09d8e5def4ff0a1297d5e9da32da47d7cd427
saulo mendes <saulomf91@hotmail.com>
2022-01-19 01:20:09 -0300

    Consertada checagem da existencia de main

--- src/main/java/benchmark.java
+++ src/main/java/benchmark.java
@@ -2,16 +2,62 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.lang.reflect.Method;
 
 public class benchmark {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Collection<Class<?>> classes = new ArrayList<Class<?>>();
 
-    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        JarFile jar = new JarFile("/home/saulo/programs/CryptoAPI-Bench/build/libs/rigorityj-samples-1.0-SNAPSHOT.jar");
+        for (Enumeration<JarEntry> entries = jar.entries(); entries.hasMoreElements() ;) {
+            JarEntry entry = entries.nextElement();
+            String file = entry.getName();
+            if (file.endsWith(".class")) {
+                String classname = file.replace('/', '.').substring(0, file.length() - 6);
+                try {
+                    Class<?> c = Class.forName(classname);
+                    classes.add(c);
+                }
+                catch (Throwable e) {
+                    System.out.println("WARNING: failed to instantiate " + classname + " from " + file);
+                }
+            }
+        }
+
+        for (Class<?> c : classes) {
+            Class<?> currentClass = Class.forName(c.getName());
+            Method[] methods = currentClass.getMethods();
+
+            int flagMain = 0;
+            for(Method method : methods){
+                //System.out.println(method.getName());
+                if (method.getName().equals("main")){
+                    flagMain++;
+                    System.out.println("Classe: " + c.getName() + "\tContém main: Sim");
+                    /*try {
+                        Method currentMethod = currentClass.getMethod("main", String.class);
+                        Object[] parametro = new Object[1];
+                        parametro[0] = "";
+                        Object resultado = currentMethod.invoke(currentClass.newInstance(), parametro);
+
+                        System.out.println("Chamada a main: " + resultado);
+
+                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+                        e.printStackTrace();
+                    }*/
+                }
+            }
+            if(flagMain == 0){
+                System.out.println("Classe: " + c.getName() + "\tContém main: Não");
+            }
+        }
+    }
+    /*public static void main(String[] args) throws ClassNotFoundException, IOException {
         List<String> classNames = new ArrayList<String>();
         ZipInputStream zip = new ZipInputStream(new FileInputStream("/home/saulo/programs/CryptoAPI-Bench/build/libs/rigorityj-samples-1.0-SNAPSHOT.jar"));
         for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
@@ -42,5 +88,5 @@ public class benchmark {
         }
         System.out.println(classNames);
 
-    }
+    }*/
 }
